9f6262ef0e4da03d2964a06c21e278e0cc9f3489
Otávio Fernandes <otaviof@gmail.com>
2019-09-22 12:51:22 +0200

    Utils.

---
+++ utils.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+)
+
+// dirExists check if a directory exists.
+func dirExists(dirPath string) bool {
+	_, err := os.Stat(dirPath)
+	return !os.IsNotExist(err)
+}
+
+// readLines read lines from file. Return error in case of having issues to read given file.
+func readLines(filePath string) ([]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return []string{}, err
+	}
+	defer f.Close()
+
+	var lines []string
+	reader := bufio.NewReader(f)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return []string{}, err
+		}
+		lines = append(lines, string(line))
+	}
+	return lines, nil
+}
---
+++ utils_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUtilsDirExists(t *testing.T) {
+	assert.True(t, dirExists("."))
+	assert.False(t, dirExists("/x/y/z/path-helper"))
+}
+
+func TestUtilsReadLines(t *testing.T) {
+	lines, err := readLines("utils_test.go")
+	assert.NoError(t, err)
+	assert.True(t, len(lines) > 0)
+}
